c30b00caf53ca657290800b8fe93a6882983ee6a
Quanto99 <scottstavran@comcast.net>
2015-10-03 14:16:08 -0400

    Failed attempt to define an exponent operator

--- ElementaryCSharp/DataTypes.cs
+++ ElementaryCSharp/DataTypes.cs
@@ -13,17 +13,31 @@ namespace DataTypesNamespace
         static double _double = 1000;
         static decimal _decimal = 1000;
 
+        // This won't compile...
+        //public static double operator ^(
+        //    double operand, double exponent)
+        //{
+        //    double result = operand;
+
+        //    while (--exponent > 0)
+        //    {
+        //        result *= operand;
+        //    }
+            
+        //    return result;
+        //}
+
         public static void Run()
         {
             try
             {
                 while (true)
                 {
-                    _float = _float * _float;
+                    _float *= _float;
                     Console.WriteLine("float:   " + _float);
-                    _double = _double * _double;
+                    _double *= _double;
                     Console.WriteLine("double:  " + _double);
-                    _decimal = _decimal * _decimal;
+                    _decimal *= _decimal;
                     Console.WriteLine("decimal: " + _decimal);
                 }
             }
