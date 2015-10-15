f1210c3464b6ef288096f587fbff66d72754ca36
Quanto99 <scottstavran@comcast.net>
2015-10-14 23:36:55 -0400

    Filescan 1st cut

---
+++ FileScan/FileInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileScan
+{
+    class FileInformation
+    {
+        public static Int64 totalsize = 0;
+
+        public static void Scan(DirectoryInfo dir, SearchOption option, string searchPattern)
+        {
+            Console.WriteLine(dir.FullName);
+
+            for (int i = 0; i < searchPattern.Length; i++)
+            {
+                FileInfo[] fiArray = dir.GetFiles(searchPattern, option);
+
+                foreach (FileInfo f in fiArray)
+                {
+                    Console.WriteLine("     {0}              {1} bytes", f.Name, f.Length);
+                    totalsize += f.Length;
+                }
+            }
+
+            // Scan subdirectories (recursively)
+            if (option == SearchOption.AllDirectories)
+            {
+                foreach (string element in Directory.GetDirectories(dir.FullName))
+                {
+                    DirectoryInfo d = new DirectoryInfo(element);
+                    Scan (d, option, searchPattern);
+                    Console.WriteLine("{0} bytes", totalsize);
+                }
+            }
+        }
+    }
+}
---
+++ FileScan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileScan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Syntax();
+                return;
+            }
+
+            DirectoryInfo d = new DirectoryInfo(args[0]);
+            FileInformation.Scan(d, SearchOption.AllDirectories, "*");
+
+            Console.WriteLine("TotalSize = {0}", FileInformation.totalsize);
+        }
+
+        public static void Syntax()
+        {
+            Console.WriteLine("Syntax:  FileScan <full directory path>");
+        }
+    }
+}
